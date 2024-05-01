f487a42a8253e40ce2d3d922ce30fd5f69c32fec
Israel Hamdan <IsraelSantos_20K@outlook.com>
2024-05-01 17:48:41 -0300

    Filtrando operações por usuário

---
+++ NaoCompraram.sql
@@ -0,0 +1,7 @@
+Select 
+	U.idUser,
+	U.loginUser 
+	From Usuarios U
+	Left join Movimentos M  on U.idUser = M.FK_idUsuario
+	Where 
+		M.idMovimento IS NULL OR M.tipo <> 'E';
---
+++ SomaDeVendasPorUsuario.sql
@@ -0,0 +1,8 @@
+Select 
+	U.idUser as id, 
+	U.loginUser as nome, 
+	SUM(M.precoUnitario) as Soma
+	From Usuarios U 
+	Left Join Movimentos M on U.idUser = M.FK_idUsuario
+	Where M.tipo = 'V'
+	Group by U.idUser, U.loginUser
